248eefdd80932cee62b9aafb892c283ec9b8f204
Mr-6ix9ine <akshayks056@gmail.com>
2024-06-30 15:59:20 +0530

    new

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -35,7 +35,7 @@ function Header() {
             About
           </li>
         </Link>
-        <Link to='/profile'>
+        <Link to='/sign-in'>
             <li className=' text-slate-700 hover:underline'> Sign in</li>
         </Link>
       </ul>
