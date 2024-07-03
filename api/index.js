f56614014f61194de2d4552201c01eb4b6c804fc
Mr-6ix9ine <akshayks056@gmail.com>
2024-07-03 16:35:49 +0530

    create listing

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import userRouter from './routes/user.route.js';
+import listingRouter from './routes/listing.route.js';
 
 dotenv.config();
 
@@ -21,7 +23,10 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
+app.use('/api/user', userRouter);
 app.use('/api/auth',authRouter);
+app.use('/api/listing', listingRouter);
+
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
